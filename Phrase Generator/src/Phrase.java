d7cae5a75c3e3d7c9922c93ba6b50527097550bd
Kaisar <bla@bla.ru>
2016-09-11 21:47:43 +0600

    Initial check in of Phrase Generator

---
+++ Phrase Generator/src/Phrase.java
@@ -0,0 +1,27 @@
+
+public class Phrase {
+	public static void main(String[] args){
+		String[] wordListOne = {"круглосуточный", "трех-звенный",
+		"30000 футовый", "взаимный", "обоюдный выигрыш", "фронтэнд", 
+		"на основе веб технологий", "проникающий", "умный", "динамичный"		
+		};
+		String[] wordListTwo = {"уполномоченный", "трудный", "чистый продукт",
+		"ориентированный", "распределенный", "центральный", "кластеризованный",
+		"фирменный", "нестандартный"
+		};
+		String[] wordListThree = {"процесс", "пункт разгрузки", "портал",
+		"выход из положения", "тип структуры", "подход", "уровень завоеванного внимания",
+		"образец"
+		};
+		int oneLength = wordListOne.length;
+		int twoLength = wordListTwo.length;
+		int threeLength = wordListThree.length;
+		
+		int rand1 = (int) (Math.random() * oneLength);
+		int rand2 = (int) (Math.random() * twoLength);
+		int rand3 = (int) (Math.random() * threeLength);
+		
+		String phrase = wordListOne[rand1] + " " + wordListTwo[rand2] + " " + wordListThree[rand3];
+		System.out.println("Всё, что вам нужно, - это " + phrase);
+	}
+}
